734bed742338857003aed57ff074623c14d3bbe8
cuddapahlakshmi <lakshmi@cortracker.com>
2024-06-18 16:23:32 +0530

    updated signup page

--- ERP_Web/src/app/loginPages/sign-up/sign-up.component.ts
+++ ERP_Web/src/app/loginPages/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BackendService } from '../../Services/BackendConnection/backend.service';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  signupform: FormGroup = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    company: new FormControl(''),
+    designation:new FormControl(''),
+    phonenumber:new FormControl(''),
+    gender:new FormControl(<number>(0)),
+    email:new FormControl(''),
+    password:new FormControl('')
+  });
 
+  submitted = false;
+  formBuilder: any;
+
+  constructor(private fb: FormBuilder , private backendservice:BackendService) {}
+  ngOnInit(): void {
+
+    this.signupform = this.fb.group(
+      {
+        firstname: ['', Validators.required],
+        lastname: ['',Validators.required],
+        company: ['',Validators.required],
+        designation: ['',Validators.required],
+        phonenumber: ['',Validators.required],
+        gender:[0],
+        email: ['', [Validators.required]],
+        password: ['',[Validators.required,Validators.minLength(6),
+            Validators.maxLength(40)]],
+      });
+  } 
+  get f(): { [key: string]: AbstractControl } {
+    return this.signupform.controls;
+  }
+  successmsg:any;
+
+  onSubmit(): void {
+  
+   this.submitted = true;
+
+    if (this.signupform.invalid) {
+      return;
+    }
+    console.log(this.signupform.getRawValue());
+    this.backendservice.postapi('api/Login/AddLogins',this.signupform.getRawValue()).subscribe(response=>{
+      this.successmsg="Saved Successfully";
+    })
+    
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.signupform.reset();
+  }
+
+  
 }
