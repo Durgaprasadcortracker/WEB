684c311445976eab653307754243f79bd5065949
cuddapahlakshmi <lakshmi@cortracker.com>
2024-06-26 11:20:57 +0530

    socila media added

--- ERP_Web/src/app/CRM_Module/Campagins/add-socialmedia/add-socialmedia.component.ts
+++ ERP_Web/src/app/CRM_Module/Campagins/add-socialmedia/add-socialmedia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-socialmedia',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './add-socialmedia.component.css'
 })
 export class AddSocialmediaComponent {
+  schedulePostForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.schedulePostForm = this.fb.group({
+      title: ['', Validators.required],
+      content: ['', Validators.required],
+      scheduleDate: ['', Validators.required],
+      scheduleTime: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.schedulePostForm.valid) {
+      const formValue = this.schedulePostForm.value;
+      const scheduledPost = {
+        title: formValue.title,
+        content: formValue.content,
+        scheduleDate: formValue.scheduleDate,
+        scheduleTime: formValue.scheduleTime
+      };
+      console.log('Scheduled Post:', scheduledPost);
+      // Here you can call a service to handle the backend API call
+    }
+  }
 }
--- ERP_Web/src/app/Services/BackendConnection/backend.service.ts
+++ ERP_Web/src/app/Services/BackendConnection/backend.service.ts
@@ -8,12 +8,14 @@ import { API_URL } from '../../utility/constants';
   providedIn: 'root'
 })
 export class BackendService {
-
+  private apiUrl = 'https://www.youtube.com/channel/UChcK61TdP5xZCrK8a79ID8w';
   private baseUrl = API_URL; 
 
   constructor(private http: HttpClient) {}
   
-
+  schedulePost(postData: any): Observable<any> {
+    return this.http.post(this.apiUrl, postData);
+  }
   getapi(url:any): Observable<any> {
     return this.http.get(`${this.baseUrl}${url}`);
   }
