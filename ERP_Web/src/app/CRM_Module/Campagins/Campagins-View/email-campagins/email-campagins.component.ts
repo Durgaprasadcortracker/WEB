0107868975f12cb75e975f30af1255f44c756337
Ravichandra <ravichandra@cortracker.com>
2024-06-26 09:24:02 +0530

    update

--- ERP_Web/src/app/CRM_Module/Campagins/Campagins-View/email-campagins/email-campagins.component.ts
+++ ERP_Web/src/app/CRM_Module/Campagins/Campagins-View/email-campagins/email-campagins.component.ts
@@ -1,70 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BackendService } from '../../../../Services/BackendConnection/backend.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-email-campagins',
   templateUrl: './email-campagins.component.html',
-  styleUrl: './email-campagins.component.css'
+  styleUrls: ['./email-campagins.component.css']
 })
-export class EmailCampaginsComponent {
+export class EmailCampaginsComponent implements OnInit {
 
-    
   page: number = 1;
   count: number = 0;
   tableSize: number = 5;
   tableSizes: any = [3, 6, 9, 12];
-  p:number=1;
-  
-  constructor(private http: BackendService) { }
+  p: number = 1;
+  searchTerm: string = '';
 
-  
-  data = {
-    records: 0
-  }
+  listOfCampagins: any[] = [];
+  filteredCampaigns: any[] = [];
   contactPage = 0
-  listOfCampagins:any
-  editData:any
+  editData: any;
+
+  constructor(
+    private http: BackendService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.getData()
+    this.getData();
   }
 
   addEmail(message: any) {
     console.log(message);
     this.contactPage = 0;
     this.editData = null;
-    this.ngOnInit()
+    this.ngOnInit();
   }
-
-  //api/Campaign/GetEmailDetails
-  //api/Campaign/GetCampaigns
 
   getData() {
     this.http.getapi('api/Campaign/GetEmailDetails').subscribe((res) => {
       console.log(res);
-      this.listOfCampagins=res.data
+      this.listOfCampagins = res.data;
+      this.filteredCampaigns = this.listOfCampagins; // Initialize filteredCampaigns with the complete list
     });
   }
- onTableDataChange(event: any) {
+
+  onTableDataChange(event: any) {
     this.page = event;
     this.getData();
   }
-  
+
   onTableSizeChange(event: any): void {
     this.tableSize = event.target.value;
     this.page = 1;
     this.getData();
   }
-  deleteEmail(ID:any){
-      this.http.deleteapi('api/Campaign/DeleteCampaign/'+ID).subscribe((res) => {
-          console.log(res);
-          this.listOfCampagins=res
-          this.ngOnInit()
-        }
-      );
-    }
- edit(data:any){
-    this.contactPage=1;
-    this.editData=data
+
+  filterCampaigns() {
+    const searchTermLower = this.searchTerm.toLowerCase();
+    this.filteredCampaigns = this.listOfCampagins.filter(campaign => {
+      return campaign.eMail.toLowerCase().includes(searchTermLower);
+    });
+  }
+
+  deleteEmail(ID: any) {
+    this.http.deleteapi('api/Campaign/DeleteCampaign/' + ID).subscribe((res) => {
+      console.log(res);
+      this.getData(); // Refresh the data after deletion
+      this.snackBar.open('Company successfully deleted!', 'Close', {
+        duration: 3000, // Snackbar stays open for 3 seconds
+      });
+    });
+  }
+
+  edit(data: any) {
+    this.contactPage = 1;
+    this.editData = data;
+    this.router.navigate(['/CRM/AddEmailConversation', data.id]);
   }
 }
--- ERP_Web/src/app/CRM_Module/Companies/company-main/company-main.component.ts
+++ ERP_Web/src/app/CRM_Module/Companies/company-main/company-main.component.ts
@@ -3,7 +3,6 @@ import { BackendService } from '../../../Services/BackendConnection/backend.serv
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
-
 @Component({
   selector: 'app-company-main',
   templateUrl: './company-main.component.html',
@@ -14,21 +13,20 @@ export class CompanyMainComponent {
   count: number = 0;
   tableSize: number = 5;
   tableSizes: any = [5, 10, 15, 20];
-  p:number=1;
+  p: number = 1;
 
-  listOfCompanys:any;
-  editData:any;
-  router: any;
-  company: any;
-  
+  listOfCompanys: any[] = [];
+  filteredCompanies: any[] = [];
+  editData: any;
+  searchTerm: string = '';
+company: any;
+
   constructor(private http: BackendService,
-    
     private snackBar: MatSnackBar,
-  ) { }
+    private router: Router) { }
 
   companyPage = 0
 
-  
   ngOnInit() {
     this.getData()
   }
@@ -39,35 +37,44 @@ export class CompanyMainComponent {
     this.editData = null;
     this.ngOnInit()
   }
-  getData(){
+
+  getData() {
     this.http.getapi('api/Company/GetCompany').subscribe((res) => {
-        console.log(res);
-        console.log
-        this.listOfCompanys=res.data
-      }
-    );
+      console.log(res);
+      this.listOfCompanys = res.data;
+      this.filterCompanies(); // Filter the data initially
+    });
   }
+
   onTableDataChange(event: any) {
     this.page = event;
     this.getData();
   }
+
   onTableSizeChange(event: any): void {
     this.tableSize = event.target.value;
     this.page = 1;
     this.getData();
   }
-  deleteCompany(ID:any){
-    this.http.deleteapi('api/Company/DeleteCompanies/'+ID).subscribe((res) => {
+
+  deleteCompany(ID: any) {
+    this.http.deleteapi('api/Company/DeleteCompanies/' + ID).subscribe((res) => {
       this.snackBar.open('Company successfully Deleted!', 'Close', {
         duration: 3000, // Snackbar stays open for 3 seconds
       });
-        console.log(res);
-        this.listOfCompanys=res
-        this.ngOnInit()
-      }
-    );
+      console.log(res);
+      this.getData(); // Refresh the data after deletion
+    });
   }
+
   edit(Id: any) {
     this.router.navigate(['/CRM/editcompany', Id]);
+  }
+
+  filterCompanies() {
+    const searchTermLower = this.searchTerm.toLowerCase();
+    this.filteredCompanies = this.listOfCompanys.filter(company => {
+      return company.companyName.toLowerCase().includes(searchTermLower);
+    });
   }
 }
